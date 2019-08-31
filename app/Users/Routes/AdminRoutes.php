821a2fd09d2219e81d6ffd6d4d285e5dbdb777ad
Ollie Read <ollie@ollieslab.io>
2019-08-31 16:53:41 +0100

    Fix error with accessing admin panel

--- app/Users/Routes/AdminRoutes.php
+++ app/Users/Routes/AdminRoutes.php
@@ -5,12 +5,13 @@ namespace Ollieread\Users\Routes;
 use Illuminate\Routing\Router;
 use Ollieread\Core\Support\Contracts\Routes;
 use Ollieread\Users\Actions\Admin as Actions;
+use Ollieread\Users\Support\Permissions;
 
 class AdminRoutes implements Routes
 {
     public function __invoke(Router $router)
     {
-        $router->middleware(['auth', 'can:admin'])->group(static function (Router $router) {
+        $router->middleware(['auth', 'can:ADMIN_MASTER'])->group(static function (Router $router) {
             $router->get('/')->name('dashboard')->uses(Actions\Dashboard::class);
             $router->get('/users')->name('user.index')->uses(Actions\Users\Index::class);
         });
