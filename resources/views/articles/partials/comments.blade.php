e37c2b03ce5e6fe62eb730ea9ad224f57ec60126
Ollie Read <ollie@ollieslab.io>
2019-09-01 11:46:15 +0100

    Remove unused partials

--- resources/views/articles/partials/comments.blade.php
+++
@@ -1,7 +0,0 @@
-<div id="article-comments" class="comments">
-
-    @if ($comments && $comments->isNotEmpty())
-        @each('articles.partials.comment', $comments, 'comment')
-    @endif
-
-</div>
