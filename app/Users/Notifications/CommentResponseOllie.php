e849faa2ee7e8cc45347c794113ebf20bfa389c6
Ollie Read <ollie@ollieslab.io>
2019-10-03 13:51:34 +0100

    Add notification email of a comment on an article

---
+++ app/Users/Notifications/CommentResponseOllie.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Ollieread\Users\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Ollieread\Articles\Models\Comment;
+
+class CommentResponseOllie extends Notification
+{
+    use Queueable;
+
+    /**
+     * @var \Ollieread\Articles\Models\Comment
+     */
+    private $comment;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param \Ollieread\Articles\Models\Comment $comment
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function toArray($notifiable): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject(trans('mail.comment-response.subject'))
+            ->line(sprintf('There has been a response to your article %s by %s.', $this->comment->article->name, $this->comment->author->username))
+            ->action('View Response', route('articles:article', $this->comment->article->slug) . '#comment-' . $this->comment->id);
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+}
