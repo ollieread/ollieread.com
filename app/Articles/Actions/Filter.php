3b280f92ad2acdf85a4b80240a805e3c47e22ca6
Ollie Read <ollie@ollieslab.io>
2019-10-05 13:39:47 +0100

    Playing around with filtering

--- app/Articles/Actions/Filter.php
+++ app/Articles/Actions/Filter.php
@@ -5,17 +5,15 @@ namespace Ollieread\Articles\Actions;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Ollieread\Articles\Operations\GetArticles;
-use Ollieread\Articles\Operations\GetCategories;
-use Ollieread\Articles\Operations\GetOrCreateTags;
-use Ollieread\Articles\Operations\GetTopics;
-use Ollieread\Articles\Operations\GetVersions;
+use Illuminate\Support\Arr;
+use Ollieread\Articles\Operations\FilterArticles;
 use Ollieread\Core\Support\Action;
 
 class Filter extends Action
 {
     public function __invoke(Request $request, ?string $filterString = null)
     {
+
         if (! $filterString) {
             return $this->createFilterAndRedirect($request);
         }
@@ -25,86 +23,73 @@ class Filter extends Action
 
     private function createFilterAndRedirect(Request $request): RedirectResponse
     {
+        // field:FN(value1 value2 value3),field:FN(value1 value2 value)
         $input = $request->only([
             'topics',
             'categories',
             'versions',
-            'tags',
-            'before',
-            'after',
-            'between',
         ]);
 
-        $uriSegment = '';
+        $filters = [];
 
         if ($input['topics']) {
-            $uriSegment .= 'topics:' . implode(',', $input['topics']) . ';';
+            $filters[] = 'topic:IN(' . implode(' ', $input['topics']) . ')';
         }
 
         if ($input['categories']) {
-            $uriSegment .= 'categories:' . implode(',', $input['categories']) . ';';
+            $filters[] = 'category:IN(' . implode(' ', $input['categories']) . ')';
         }
 
-        if ($input['versions']) {
-            $uriSegment .= 'versions:' . implode(',', $input['versions']) . ';';
+        if ($input['categories']) {
+            $filters[] = 'version:IN(' . implode(' ', $input['versions']) . ')';
         }
 
-        if ($input['tags']) {
-            $uriSegment .= 'tags:' . implode(',', $input['tags']) . ';';
-        }
-
-        if ($input['before']) {
-            $uriSegment .= 'before:' . $input['before'] . ';';
-        }
-
-        if ($input['after']) {
-            $uriSegment .= 'after:' . $input['after'] . ';';
-        }
-
-        if ($input['between']) {
-            $uriSegment .= 'between:' . implode(',', $input['between']) . ';';
-        }
+        $uriSegment = urlencode(implode(',', $filters));
 
         return $this->response()->redirectToRoute('articles:search', substr($uriSegment, -1));
     }
 
     private function filterAndGetResults(string $filterString): Response
     {
-        $filterParts  = explode(';', $filterString);
-        $filterParsed = [];
+        $filters = $this->parseFilters($filterString);
 
-        foreach ($filterParts as $part) {
-            $subparts                   = explode(':', $part);
-            $filterParsed[$subparts[0]] = explode(',', $subparts[1]);
+        if ($filters) {
+            $articleFiltering = new FilterArticles;
+            $fieldFiltering   = $filters['field'] ?? [];
+
+            if (isset($fieldFiltering['categories'])) {
+                $articleFiltering->setCategories($fieldFiltering['categories']['args'])->setCategoryFunc($fieldFiltering['categories']['func']);
+            }
+
+            if (isset($fieldFiltering['topics'])) {
+                $articleFiltering->setTopics(['topics']['args'])->setTopicFunc($fieldFiltering['topics']['func']);
+            }
+
+            if (isset($fieldFiltering['versions'])) {
+                $articleFiltering->setVersions($fieldFiltering['versions']['args'])->setVersionFunc($fieldFiltering['versions']['func']);
+            }
         }
-
-        $categories = $topics = $versions = $tags = null;
-
-        if (isset($filterParsed['categories'])) {
-            $categories = (new GetCategories)->setSlugs($filterParsed['categories'])->perform();
-        }
-
-        if (isset($filterParsed['topics'])) {
-            $topics = (new GetTopics)->setSlugs($filterParsed['topics'])->perform();
-        }
-
-        if (isset($filterParsed['versions'])) {
-            $versions = (new GetVersions)->setSlugs($filterParsed['versions'])->perform();
-        }
-
-        if (isset($filterParsed['tags'])) {
-            $tags = (new GetOrCreateTags)->setNames($filterParsed['tags'])->perform();
-        }
-
-        $articles = (new GetArticles)
-            ->setTopics($topics)
-            ->setTags($tags)
-            ->setVersions($versions)
-            ->setCategories($categories)
-            ->perform();
 
         $filters = compact('categories', 'topics', 'versions', 'tags');
 
         return $this->response()->view('articles.listing', compact('articles', 'filters'));
+    }
+
+    private function parseFilters(string $filterString)
+    {
+        $parts   = explode(',', urldecode($filterString));
+        $filters = [];
+
+        foreach ($parts as $part) {
+            preg_match('/(?<field>[a-z\-0-9]*):(?<func>[A-Z]*)\((?<args>[a-z0-9\-\s]*)\)/', $part, $matches);
+            $filterParts = Arr::only($matches, ['field', 'func', 'args']);
+
+            if (count($filterParts) === 3) {
+                $filterParts['args']                     = explode(' ', $filterParts['args']);
+                $filters['field'][$filterParts['field']] = $filterParts;
+            }
+        }
+
+        return $filters;
     }
 }
