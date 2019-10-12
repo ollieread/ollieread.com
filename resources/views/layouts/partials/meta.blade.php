c58eea351be27d614b148badcc02bd8abdb898ef
Ollie Read <ollie@ollieslab.io>
2019-10-12 21:09:21 +0100

    Fix GA exclusion for admin routes

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -1,7 +1,7 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-19364030-1"></script>
     <script>
-        if (! window.location.pathname.startsWith('admin')) {
+        if (! window.location.pathname.startsWith('/admin')) {
             window.dataLayer = window.dataLayer || [];
 
             function gtag() {
